067e5df0ec1c44aa573459bd73884d953a513e6c
Enes Başkaya <enesbaskaya08@gmail.com>
2018-12-31 16:14:44 +0200

    Sol menu eklendi

---
+++ app/src/main/java/com/enes_08/leftmenu/LeftMenu.kt
@@ -0,0 +1,73 @@
+package com.enes_08.leftmenu
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+
+class LeftMenu : Fragment(), View.OnClickListener {
+
+
+
+    lateinit var llAnasayfa: LinearLayout
+    lateinit var llhakkimizda: LinearLayout
+    lateinit var llAyarlar: LinearLayout
+    lateinit var llCikisYap: LinearLayout
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        var view = inflater.inflate(R.layout.fragment_left_menu, container, false)
+
+        llAnasayfa= view.findViewById<LinearLayout>(R.id.llAnasayfa)
+        llhakkimizda= view.findViewById<LinearLayout>(R.id.llhakkimizda)
+        llAyarlar=view.findViewById<LinearLayout>(R.id.llAyarlar)
+        llCikisYap= view.findViewById<LinearLayout>(R.id.llCikisYap)
+
+
+        llAnasayfa.setOnClickListener (this )
+        llhakkimizda.setOnClickListener(this)
+        llAyarlar.setOnClickListener(this)
+        llCikisYap.setOnClickListener(this)
+
+        return view
+    }
+
+
+    override fun onClick(v: View?) {
+
+
+        when(v){
+            llAnasayfa->{
+
+                Toast.makeText(getActivity(),"AnaSayfa Tıkalndı",Toast.LENGTH_SHORT).show()
+
+            }
+
+            llAyarlar->{
+                Toast.makeText(getActivity(),"Ayarlar Tıkalndı",Toast.LENGTH_SHORT).show()
+
+
+            }
+
+            llhakkimizda->{
+                Toast.makeText(getActivity(),"Hakkımızda Tıkalndı",Toast.LENGTH_SHORT).show()
+
+
+            }
+
+            llCikisYap->{
+
+                Toast.makeText(getActivity(),"Çıkış yap Tıkalndı",Toast.LENGTH_SHORT).show()
+
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/enes_08/leftmenu/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.enes_08.leftmenu
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v4.view.GravityCompat
+
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    var fm:FragmentManager?=null
+    var tr:FragmentTransaction?=null;
+    lateinit var mleftMenu:LeftMenu
+    var ivMenu: ImageView? = null
+    var mtoolBar: Toolbar? = null
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        fm=supportFragmentManager;
+        var mleftMenu= LeftMenu()
+        mtoolBar = findViewById(R.id.toolBar);
+
+
+        ivMenu = mtoolBar?.findViewById(R.id.ivBack)
+
+        ivMenu?.setOnClickListener(this)
+
+
+
+
+
+        tr=fm?.beginTransaction();
+
+        tr?.add(R.id.frame,mleftMenu)
+        tr?.commit()
+
+
+        
+
+    }
+
+
+    override fun onClick(v: View?) {
+
+        when(v){
+            ivMenu->{
+                val drawer = findViewById(R.id.drawerLayout) as DrawerLayout
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START)
+                } else {
+                    drawer.openDrawer(GravityCompat.START)
+                }
+            }
+        }
+    }
+}
